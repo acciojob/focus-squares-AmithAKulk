7b47f10903a6ceae392e3cab581dcd54639ce2de
AmithAKulk <95955499+AmithAKulk@users.noreply.github.com>
2023-11-28 16:41:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,36 @@
 //your JS code here. If required.
-function changeColor(id){
-	for(let i=1;i<=3;i++){
-		if (i !== id) {
-			// Change its color
-			document.getElementById("square" + i).style.backgroundColor = "#6F4E37"; //coffee
-		}
+function focusSquare(){
+	let square1 = document.getElementById("square1");
+	let square2 = document.getElementById("square2");
+	let square3 = document.getElementById("square3");
+
+	function resetColors(){
+		square1.style.backgroundColor = "#E6E6FA";
+		square2.style.backgroundColor = "#E6E6FA";
+		square3.style.backgroundColor = "#E6E6FA";
 	}
+
+	square1.addEventListener("mouseover",function() {
+
+		square2.style.backgroundColor = "#6F4E37";
+		square3.style.backgroundColor = "#6F4E37";
+	});
+
+	square2.addEventListener("mouseover",function() {
+
+		square1.style.backgroundColor = "#6F4E37";
+		square3.style.backgroundColor = "#6F4E37";
+	});
+
+	square3.addEventListener("mouseover",function() {
+
+		square1.style.backgroundColor = "#6F4E37";
+		square2.style.backgroundColor = "#6F4E37";
+	});
+
+	[square1 , square2 , square3].forEach((square) =>{
+		square.addEventListener("mouseout" , resetColors);
+	});
+	
 }
-function resetColor(id){
-	for(let i=1;i<=3;i++){
-		if (i !== id) {
-			document.getElementById("square" + i).style.backgroundColor = "#E6E6FA"; //lavender
-		}
-	}
-}
+focusSquare();
